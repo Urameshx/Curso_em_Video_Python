c3145b3654fc08bafa1b837379012963a3ac58c2
lucas.henrique2@fatec.sp.gov.br <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>
2021-12-18 20:24:16 -0300

    Curso em video - Python

--- exercicios/ex04.py
+++ exercicios/ex04.py
@@ -5,5 +5,8 @@ print('Só tem espaços?', palavra.isspace())
 print('É um número?', palavra.isnumeric())
 print('É alfabético?', palavra.isalpha())
 print('É alfanumérico?', palavra.isalnum())
+print('Está em letras maiúsculas?', palavra.isupper())
+print('Está em letras minúsculas?', palavra.islower())
+print('Está capitalizada?', palavra.istitle())
 
-print(palavra.isupper())
+
