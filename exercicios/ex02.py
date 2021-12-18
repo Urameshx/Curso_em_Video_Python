5cb2868fec82eb85db279a54dfd82a638de46883
lucas.henrique2@fatec.sp.gov.br <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>
2021-12-18 20:15:44 -0300

    Curso em video - Python

---
+++ exercicios/ex02.py
@@ -0,0 +1,3 @@
+# TODO : Respondendo ao Usuário
+nome = input('Digite seu nome: ')
+print(f'É um prazer te conhecer, {nome}!')
---
+++ exercicios/ex04.py
@@ -0,0 +1,9 @@
+# TODO : Dissecando uma variável
+palavra = input('Digite algo: ')
+print(f'O tipo primitivo desse valor é {type(palavra)}')
+print('Só tem espaços?', palavra.isspace())
+print('É um número?', palavra.isnumeric())
+print('É alfabético?', palavra.isalpha())
+print('É alfanumérico?', palavra.isalnum())
+
+print(palavra.isupper())
