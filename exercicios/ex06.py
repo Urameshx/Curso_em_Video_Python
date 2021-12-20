8977ee516b1a4a3ad0fb400d76a0da2ff29147b2
lucas.henrique2@fatec.sp.gov.br <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>
2021-12-20 18:30:56 -0300

    Curso em video - Python

---
+++ exercicios/ex06.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+num = int(input('Digite um número: '))
+
+dobro = num * 2
+triplo = num * 3
+raiz = sqrt(num)
+
+print(f"O dobro de {num} vale {dobro}")
+print(f"O triplo de {num} vale {triplo}")
+print(f"A raiz quadrada de {num} é igual a {raiz:.2f}")
