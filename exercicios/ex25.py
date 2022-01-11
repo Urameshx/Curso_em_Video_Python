2bce3f8ddde14e4955986168ab72338be3e3caf7
lucas.henrique2@fatec.sp.gov.br <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>
2022-01-11 20:13:51 -0300

    Curso em video - Python

---
+++ exercicios/ex25.py
@@ -0,0 +1,4 @@
+#Procurando uma string dentro de outra string
+nome = input('Qual o seu nome completo: ').strip()
+print(f'Seu nome tem Silva? {"SILVA" in nome.upper()}')
+
---
+++ exercicios/ex26.py
@@ -0,0 +1,5 @@
+#Primeira e última ocorrência de uma string
+frase = input('Digite uma frase: ').upper().strip()
+print(f'A letra A aparece {frase.count("A")} vezes na frase')
+print(f'A primeira letra A apareceu na posição {frase.find("A")}')
+print(f'A ultima letra A apareceu na posição {frase.rfind("A")}')
